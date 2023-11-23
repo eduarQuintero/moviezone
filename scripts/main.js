45c02da4b549ed9877f87ab464078ad08c1e2f87
eduar <eduar.curi73@outlook.com>
2023-11-23 00:39:37 -0500

    Nav bar modification

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,13 @@
 const menuButtonOpen = document.getElementById("menu-button-open")
 const menuButtonClose = document.getElementById("menu-button-close")
 const sectionNav = document.getElementById("section-nav")
-const body = document.getElementById("body")
 
+// SHOW MENU BAR
 menuButtonOpen.addEventListener("click", () => {
     sectionNav.classList.add("aparicion")
-    body.style.opacity = "0.7"
 })
 
+// HIDE MENU BAR
 menuButtonClose.addEventListener("click", () => {
     sectionNav.classList.remove("aparicion")
-    body.style.opacity = "1"
 })
